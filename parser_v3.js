5e96e0c15ed52c6b26456ab42f49c6162ed0ba5d
minhogang <minhogang@yahoo.com>
2019-03-01 14:05:06 +1000

    Start requisite parsing

--- parser_v3.js
+++ parser_v3.js
@@ -1,3 +1,8 @@
+// if (process.argv.length != 4) {
+//     console.log("Parser must be supplied with two arguments: Input XML filename, Output XML filename");
+//     process.exit(1);
+// }
+
 const xml2js = require('xml2js');
 const fs = require('fs');
 
@@ -5,6 +10,9 @@ const rootNode = {COURSES: []};
 
 const OLD_COURSE_NAME = 'OU.SAMPLE.EXPORT2.xml'; //Course to read from
 const NEW_COURSE_NAME = 'newuogcourses.xml'; //The name of the new XML file
+
+const MATH_REQUISITE = '# Take MA-084B MA-085 MA-085/LI MA-085/LII MA-085A MA-085B MA-089 MA-9110 MA-9161A MA-110 MA-115(1529) MA-151 MA-161A MA-161B MA-165 MA-203 or MA-204';
+const ENGLISH_REQUISITE = 'Take EN-085 EN-9100 EN-9110 EN-100 EN-101B EN-109 EN-110 or EN-111; Minimum grade A,B,C,D,F,I,NC,P2,A2;';
 
 const parseXML = () => {
         const data = fs.readFileSync(OLD_COURSE_NAME);
@@ -17,6 +25,8 @@ const parseXML = () => {
                     const words = line['CRS.DESC_MS'][0]['CRS.DESC'][0].split(/\s+/).filter(word => word != '');
                     newDescription = newDescription === null ? words.join(' ') : 
                     newDescription + ' ' + words.join(' ');
+                    const requisites = course['CRS.REQUISITES.SPECS_MV'][0]['CRS.REQUISITES.SPECS_MS'][0]['CRS.REQUISITES.SPECS'];
+                    console.log(requisites);
                 }
                 let newDescriptionObject = [{'CRS.DESC_MS': [{'CRS.DESC': [newDescription]}]}];
                 courseDeepClone['CRS.DESC_MV'] = newDescriptionObject;
